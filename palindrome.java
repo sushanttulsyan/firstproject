552d40e8b803c571c0163ffe8d591d9746736bd6
sushanttulsyan <59223305+sushanttulsyan@users.noreply.github.com>
2019-12-25 16:14:44 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,16 +1,25 @@
 class PalindromeExample{  
  public static void main(String args[]){  
-  int r,sum=0,temp;    
+  long r,sum=0,temp;    
   int n=454;//It is the number variable to be checked for palindrome  
   
   temp=n;    
-  while(n>0){    
-   r=n%10;  //getting remainder  
-   sum=(sum*10)+r;    
-   n=n/10;    
+  while(n>0)
+  
+  {    
+   r=n%10;  //getting remainder
+   
+   sum=sum*10+r;
+   
+   
+   n=n/10;
+   
+   
+   
   }    
-  if(temp==sum)    
-   System.out.println("palindrome number ");    
+  if(temp==sum)  {
+   System.out.println("palindrome number "); 
+  }
   else    
    System.out.println("not palindrome");    
 }  
