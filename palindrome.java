8b72992853f18e014fb277ac2ec82bfc657e4619
sushanttulsyan <59223305+sushanttulsyan@users.noreply.github.com>
2019-12-25 18:24:31 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,27 +1,10 @@
-public void disconnect(){
-  //System.err.println(this+":disconnect "+io+" "+connected);
-  //Thread.dumpStack();
-  try{
-    synchronized(this){
-      if(!connected){
-        return;
-      }
-      connected=false;
+public void doSomething() {
+    String result = doSomethingElse();
+    if (result.equalsIgnoreCase("Success")) 
+        // success
     }
-    close();
-    eof_remote=eof_local=true;
-    thread=null;
-    try{
-      if(io!=null){
-        io.close();
-      }
-    }
-    catch(Exception e){
-      //e.printStackTrace();
-    }
-    // io=null;
-  }
-  finally{
-    Channel.del(this);
-  }
+}
+ 
+private String doSomethingElse() {
+    return null;
 }
